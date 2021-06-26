b2db01252348e8133ba3520572e6199807e51e9c
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2021-06-26 17:21:39 +0530

    minor changes

--- lib/ui/views/bluetoothDiscovery/bluetooth_discovery_view.dart
+++ lib/ui/views/bluetoothDiscovery/bluetooth_discovery_view.dart
@@ -185,191 +185,192 @@ class BluetoothDiscoveryView extends StatelessWidget {
               ),
             ],
           ),
-          body: Container(
-            height: height,
-            width: width,
-            child: Flex(
-              direction: Axis.vertical,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: height / 50,
-                ),
-                Container(
-                  height: height / 5,
-                  width: width / 3,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/bluetooth.png",
+          body: SingleChildScrollView(
+            child: Container(
+              width: width,
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: height / 5,
+                    width: width / 3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          "assets/bluetooth.png",
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: height / 15,
-                ),
-                Container(
-                  width: width / 1.2,
-                  height: height / 12,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Choose your device",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: height / 30,
-                          fontFamily: 'Noah',
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        height: height / 300,
-                      ),
-                      Text(
-                        "Select Bluetooth",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: height / 45,
-                          fontFamily: 'Noah',
-                          color: Colors.white70,
-                        ),
-                      ),
-                    ],
+                  SizedBox(
+                    height: height / 15,
                   ),
-                ),
-                SizedBox(
-                  height: height / 80,
-                ),
-                Container(
-                  color: Colors.white70,
-                  height: 1,
-                  width: width / 1.2,
-                ),
-                SizedBox(
-                  height: height / 80,
-                ),
-                model.bluetoothList.length != 0
-                    ? Container(
-                        width: width / 1.2,
-                        height: height / 3.2,
-                        child: ListView.builder(
-                          itemCount: model.bluetoothList.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: width / 20,
-                                vertical: height / 100,
-                              ),
-                              child: InkWell(
-                                onTap: () {
-                                  /// Pass the bluetooth ssid to next page and connect to it
-                                  print(model.bluetoothList[index].address);
-                                  model.goToBluetoothPage(
-                                      model.bluetoothList[index]);
-                                },
-                                child: Container(
-                                  width: width / 1.2,
-                                  height: height / 13,
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Icon(
-                                        Icons.bluetooth,
-                                        color: Colors.white70,
-                                        size: height / 30,
-                                      ),
-                                      SizedBox(
-                                        width: width / 20,
-                                      ),
-                                      Text(
-                                        model.bluetoothList[index].name.trim(),
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: height / 37,
-                                          fontFamily: 'Noah',
-                                          color: Colors.white70,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    : Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical:
-                              model.searchOngoing ? height / 10 : height / 8,
-                        ),
-                        child: Container(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              model.searchOngoing
-                                  ? CircularProgressIndicator(
-                                      strokeWidth: 2,
-                                      valueColor: AlwaysStoppedAnimation<Color>(
-                                          Colors.white),
-                                    )
-                                  : Container(),
-                              Text(
-                                model.searchOngoing
-                                    ? "Searching for available devices"
-                                    : "Please Refresh",
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: height / 35,
-                                  fontFamily: 'Noah',
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
+                  Container(
+                    width: width / 1.2,
+                    height: height / 12,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Choose your device",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: height / 30,
+                            fontFamily: 'Noah',
+                            color: Colors.white,
                           ),
                         ),
-                      ),
-                SizedBox(
-                  height: model.searchOngoing ? height / 80 : 0.0,
-                ),
-                Container(
-                  color: Colors.white70,
-                  height: 1,
-                  width: width / 1.2,
-                ),
-                Text(
-                  "\n\nor\n",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 17,
-                    fontFamily: 'Noah',
-                    color: Colors.white,
+                        SizedBox(
+                          height: height / 300,
+                        ),
+                        Text(
+                          "Select Bluetooth",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: height / 45,
+                            fontFamily: 'Noah',
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                InkWell(
-                  onTap: () {
-                    print("Hello");
-                    _modalBottomSheetMenu();
-                  },
-                  child: Text(
-                    "Enter MAC manually?",
+                  SizedBox(
+                    height: height / 80,
+                  ),
+                  Container(
+                    color: Colors.white70,
+                    height: 1,
+                    width: width / 1.2,
+                  ),
+                  SizedBox(
+                    height: height / 80,
+                  ),
+                  model.bluetoothList.length != 0
+                      ? Container(
+                          width: width / 1.2,
+                          height: height / 3.2,
+                          child: ListView.builder(
+                            itemCount: model.bluetoothList.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: width / 20,
+                                  vertical: height / 100,
+                                ),
+                                child: InkWell(
+                                  onTap: () {
+                                    /// Pass the bluetooth ssid to next page and connect to it
+                                    print(model.bluetoothList[index].address);
+                                    model.goToBluetoothPage(
+                                        model.bluetoothList[index]);
+                                  },
+                                  child: Container(
+                                    width: width / 1.2,
+                                    height: height / 13,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          Icons.bluetooth,
+                                          color: Colors.white70,
+                                          size: height / 30,
+                                        ),
+                                        SizedBox(
+                                          width: width / 20,
+                                        ),
+                                        Text(
+                                          model.bluetoothList[index].name
+                                              .trim(),
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: height / 37,
+                                            fontFamily: 'Noah',
+                                            color: Colors.white70,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical:
+                                model.searchOngoing ? height / 10 : height / 8,
+                          ),
+                          child: Container(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                model.searchOngoing
+                                    ? CircularProgressIndicator(
+                                        strokeWidth: 2,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Colors.white),
+                                      )
+                                    : Container(),
+                                Text(
+                                  model.searchOngoing
+                                      ? "Searching for available devices"
+                                      : "Please Refresh",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: height / 35,
+                                    fontFamily: 'Noah',
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                  SizedBox(
+                    height: model.searchOngoing ? height / 80 : 0.0,
+                  ),
+                  Container(
+                    color: Colors.white70,
+                    height: 1,
+                    width: width / 1.2,
+                  ),
+                  Text(
+                    "\n\nor\n",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
-                      fontSize: 20,
+                      fontSize: 17,
                       fontFamily: 'Noah',
                       color: Colors.white,
-                      decoration: TextDecoration.underline,
                     ),
                   ),
-                ),
-              ],
+                  InkWell(
+                    onTap: () {
+                      print("Hello");
+                      _modalBottomSheetMenu();
+                    },
+                    child: Text(
+                      "Enter MAC manually?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        fontFamily: 'Noah',
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
--- lib/ui/views/startUp/startup_viewModel.dart
+++ lib/ui/views/startUp/startup_viewModel.dart
@@ -2,6 +2,8 @@ import 'package:air_purifier/app/locator.dart';
 import 'package:air_purifier/app/router.gr.dart';
 import 'package:air_purifier/services/firestore_service.dart';
 import 'package:air_purifier/services/streaming_shared_preferences_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info/device_info.dart';
 import 'package:stacked/stacked.dart';
 import 'package:air_purifier/services/authentication_service.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -43,6 +45,45 @@ class StartUpViewModel extends BaseViewModel {
         } else {
           _navigationService.replaceWith(Routes.loginView);
         }
+        Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+          return <String, dynamic>{
+            'version.securityPatch': build.version.securityPatch,
+            'version.sdkInt': build.version.sdkInt,
+            'version.release': build.version.release,
+            'version.previewSdkInt': build.version.previewSdkInt,
+            'version.incremental': build.version.incremental,
+            'version.codename': build.version.codename,
+            'version.baseOS': build.version.baseOS,
+            'board': build.board,
+            'bootloader': build.bootloader,
+            'brand': build.brand,
+            'device': build.device,
+            'display': build.display,
+            'fingerprint': build.fingerprint,
+            'hardware': build.hardware,
+            'host': build.host,
+            'id': build.id,
+            'manufacturer': build.manufacturer,
+            'model': build.model,
+            'product': build.product,
+            'supported32BitAbis': build.supported32BitAbis,
+            'supported64BitAbis': build.supported64BitAbis,
+            'supportedAbis': build.supportedAbis,
+            'tags': build.tags,
+            'type': build.type,
+            'isPhysicalDevice': build.isPhysicalDevice,
+            'androidId': build.androidId,
+            'systemFeatures': build.systemFeatures,
+          };
+        }
+
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        deviceInfo.androidInfo.then((value) {
+          FirebaseFirestore.instance
+              .collection("Device Information")
+              .doc("${value.id}")
+              .set(_readAndroidBuildData(value));
+        });
       },
     );
   }
